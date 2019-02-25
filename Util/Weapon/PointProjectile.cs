363df6769c334ef3fd7641ebda49b34692d8e3fc
Vincent Miller <vmiller0725@gmail.com>
2019-02-24 23:57:17 -0500

    Added radius option to PointProjectile, so it can use a SphereCast.

--- Util/Weapon/PointProjectile.cs
+++ Util/Weapon/PointProjectile.cs
@@ -19,15 +19,30 @@ namespace SBR {
         [Tooltip("Extra length to raycast each frame, to compensate for other objects' velocity.")]
         public float offset = 0;
 
+        /// <summary>
+        /// Radius of the projectile. If > 0, movement will use a SphereCast (otherwise LineCast).
+        /// </summary>
+        [Tooltip("Radius of the projectile. If > 0, movement will use a SphereCast (otherwise LineCast).")]
+        public float radius;
+
         protected virtual void Update() {
             if (velocity.sqrMagnitude > 0) {
                 Vector3 oldPosition = transform.position;
                 transform.position += velocity * Time.deltaTime;
+                
+                if (radius <= 0) {
+                    if (Physics.Linecast(oldPosition - velocity.normalized * offset, 
+                        transform.position,  out var hit,  hitMask,  triggerInteraction)) {
 
-                RaycastHit hit;
+                        OnHitCollider(hit.collider, hit.point);
+                    }
+                } else {
+                    var dir = transform.position - oldPosition;
+                    if (Physics.SphereCast(oldPosition - velocity.normalized * offset, radius, 
+                        dir, out var hit, dir.magnitude + offset, hitMask, triggerInteraction)) {
 
-                if (Physics.Linecast(oldPosition - velocity.normalized * offset, transform.position, out hit, hitMask, triggerInteraction)) {
-                    OnHitCollider(hit.collider, hit.point);
+                        OnHitCollider(hit.collider, hit.point);
+                    }
                 }
             }
         }
