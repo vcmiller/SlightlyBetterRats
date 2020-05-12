81742efcd9a49f81227d2b77e2d726c0976c6596
Vincent Miller <vmiller0725@gmail.com>
2020-05-11 19:29:06 -0700

    Improved ApplyTransform utility.
    - Can now execute on multiple objects at once.
    - Fixed bug with scale getting flipped sometimes.

--- Editor/Util/ApplyTransform.cs
+++ Editor/Util/ApplyTransform.cs
@@ -20,9 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -30,28 +28,29 @@ namespace SBR.Editor {
     public static class ApplyTransform {
         [MenuItem("GameObject/Apply Transform/Position")]
         public static void ApplyPosition() {
-            var obj = Selection.activeGameObject;
-            if (obj && obj.transform.position != Vector3.zero) {
-                RecordUndo(obj, "Apply Position");
-                Vector3[] childPositions = new Vector3[obj.transform.childCount];
-                for (int i = 0; i < obj.transform.childCount; i++) {
-                    var child = obj.transform.GetChild(i);
-                    childPositions[i] = child.position;
-                }
+            foreach (var obj in Selection.gameObjects) {
+                if (obj && obj.transform.position != Vector3.zero) {
+                    RecordUndo(obj, "Apply Position");
+                    Vector3[] childPositions = new Vector3[obj.transform.childCount];
+                    for (int i = 0; i < obj.transform.childCount; i++) {
+                        var child = obj.transform.GetChild(i);
+                        childPositions[i] = child.position;
+                    }
 
-                Vector3 offset = obj.transform.InverseTransformVector(obj.transform.position);
-                obj.transform.position = Vector3.zero;
+                    Vector3 offset = obj.transform.InverseTransformVector(obj.transform.position);
+                    obj.transform.position = Vector3.zero;
 
-                for (int i = 0; i < obj.transform.childCount; i++) {
-                    obj.transform.GetChild(i).position = childPositions[i];
-                }
+                    for (int i = 0; i < obj.transform.childCount; i++) {
+                        obj.transform.GetChild(i).position = childPositions[i];
+                    }
 
-                foreach (var col in obj.GetComponents<Collider>()) {
-                    SetColliderOffset(col, GetColliderOffset(col) + offset);
-                }
+                    foreach (var col in obj.GetComponents<Collider>()) {
+                        SetColliderOffset(col, GetColliderOffset(col) + offset);
+                    }
 
-                foreach (var col in obj.GetComponents<Collider2D>()) {
-                    col.offset += (Vector2)offset;
+                    foreach (var col in obj.GetComponents<Collider2D>()) {
+                        col.offset += (Vector2)offset;
+                    }
                 }
             }
         }
@@ -85,53 +84,54 @@ namespace SBR.Editor {
 
         [MenuItem("GameObject/Apply Transform/Rotation")]
         public static void ApplyRotation() {
-            var obj = Selection.activeGameObject;
-            if (obj && obj.transform.rotation != Quaternion.identity) {
-                RecordUndo(obj, "Apply Rotation");
+            foreach (var obj in Selection.gameObjects) {
+                if (obj && obj.transform.rotation != Quaternion.identity) {
+                    RecordUndo(obj, "Apply Rotation");
 
-                (Quaternion rot, Vector3 pos)[] childRotations = new (Quaternion, Vector3)[obj.transform.childCount];
-                for (int i = 0; i < obj.transform.childCount; i++) {
-                    var child = obj.transform.GetChild(i);
-                    childRotations[i].rot = child.rotation;
-                    childRotations[i].pos = child.position;
-                }
-
-                Quaternion offset = obj.transform.rotation;
-                obj.transform.rotation = Quaternion.identity;
-
-                for (int i = 0; i < obj.transform.childCount; i++) {
-                    var child = obj.transform.GetChild(i);
-                    child.rotation = childRotations[i].rot;
-                    child.position = childRotations[i].pos;
-                }
-
-                foreach (var col in obj.GetComponents<Collider>()) {
-                    SetColliderOffset(col, offset * GetColliderOffset(col));
-                    switch (col) {
-                        case BoxCollider box:
-                            box.size = offset * box.size;
-                            break;
-                        case CapsuleCollider capsule:
-                            capsule.direction = RotateCapuleDirection(capsule.direction, offset);
-                            break;
+                    (Quaternion rot, Vector3 pos)[] childRotations = new (Quaternion, Vector3)[obj.transform.childCount];
+                    for (int i = 0; i < obj.transform.childCount; i++) {
+                        var child = obj.transform.GetChild(i);
+                        childRotations[i].rot = child.rotation;
+                        childRotations[i].pos = child.position;
                     }
-                }
 
-                foreach (var col in obj.GetComponents<Collider2D>()) {
-                    col.offset = offset * col.offset;
-                    switch (col) {
-                        case BoxCollider2D box:
-                            box.size = offset * box.size;
-                            break;
-                        case CapsuleCollider2D capsule:
-                            capsule.direction = RotateCapuleDirection2D(capsule.direction, offset);
-                            break;
-                        case EdgeCollider2D edge:
-                            edge.points = RotatePoints(edge.points, offset);
-                            break;
-                        case PolygonCollider2D poly:
-                            poly.points = RotatePoints(poly.points, offset);
-                            break;
+                    Quaternion offset = obj.transform.rotation;
+                    obj.transform.rotation = Quaternion.identity;
+
+                    for (int i = 0; i < obj.transform.childCount; i++) {
+                        var child = obj.transform.GetChild(i);
+                        child.rotation = childRotations[i].rot;
+                        child.position = childRotations[i].pos;
+                    }
+
+                    foreach (var col in obj.GetComponents<Collider>()) {
+                        SetColliderOffset(col, offset * GetColliderOffset(col));
+                        switch (col) {
+                            case BoxCollider box:
+                                box.size = offset * box.size;
+                                break;
+                            case CapsuleCollider capsule:
+                                capsule.direction = RotateCapuleDirection(capsule.direction, offset);
+                                break;
+                        }
+                    }
+
+                    foreach (var col in obj.GetComponents<Collider2D>()) {
+                        col.offset = offset * col.offset;
+                        switch (col) {
+                            case BoxCollider2D box:
+                                box.size = offset * box.size;
+                                break;
+                            case CapsuleCollider2D capsule:
+                                capsule.direction = RotateCapuleDirection2D(capsule.direction, offset);
+                                break;
+                            case EdgeCollider2D edge:
+                                edge.points = RotatePoints(edge.points, offset);
+                                break;
+                            case PolygonCollider2D poly:
+                                poly.points = RotatePoints(poly.points, offset);
+                                break;
+                        }
                     }
                 }
             }
@@ -194,72 +194,73 @@ namespace SBR.Editor {
 
         [MenuItem("GameObject/Apply Transform/Scale")]
         public static void ApplyScale() {
-            var obj = Selection.activeGameObject;
-            if (obj && obj.transform.localScale != Vector3.one) {
-                RecordUndo(obj, "Apply Scale");
-                Vector3 oldScale = obj.transform.localScale;
-                obj.transform.localScale = Vector3.one;
+            foreach (var obj in Selection.gameObjects) {
+                if (obj && obj.transform.localScale != Vector3.one) {
+                    RecordUndo(obj, "Apply Scale");
+                    Vector3 oldScale = obj.transform.localScale;
+                    obj.transform.localScale = Vector3.one;
 
-                for (int i = 0; i < obj.transform.childCount; i++) {
-                    var child = obj.transform.GetChild(i);
-                    Vector3 v = child.transform.localPosition;
-                    v.x *= oldScale.x;
-                    v.y *= oldScale.y;
-                    v.z *= oldScale.z;
-                    child.transform.localPosition = v;
+                    for (int i = 0; i < obj.transform.childCount; i++) {
+                        var child = obj.transform.GetChild(i);
+                        Vector3 v = child.transform.localPosition;
+                        v.x *= oldScale.x;
+                        v.y *= oldScale.y;
+                        v.z *= oldScale.z;
+                        child.transform.localPosition = v;
 
-                    v = child.transform.localScale;
-                    var map = GetScaleMapping(child.transform.localRotation);
-                    var sr = map * oldScale;
-                    v.x *= sr.x;
-                    v.y *= sr.y;
-                    v.z *= sr.z;
-                    child.transform.localScale = v;
-                }
-
-                foreach (var col in obj.GetComponents<Collider>()) {
-                    var colOffset = GetColliderOffset(col);
-                    colOffset.Scale(oldScale);
-                    SetColliderOffset(col, colOffset);
-                    switch (col) {
-                        case BoxCollider box:
-                            Vector3 size = box.size;
-                            size.Scale(oldScale);
-                            box.size = size;
-                            break;
-                        case SphereCollider sphere:
-                            sphere.radius *= Mathf.Max(oldScale.x, oldScale.y, oldScale.z);
-                            break;
-                        case CapsuleCollider capsule:
-                            Vector3 dir = GetCapsuleDirection(capsule.direction);
-                            capsule.height *= Vector3.Dot(oldScale, dir);
-                            Vector3 v = Vector3.ProjectOnPlane(oldScale, dir);
-                            capsule.radius *= Mathf.Max(v.x, v.y, v.z);
-                            break;
+                        v = child.transform.localScale;
+                        var map = GetScaleMapping(child.transform.localRotation);
+                        var sr = map.MultiplyVector(oldScale);
+                        v.x *= sr.x;
+                        v.y *= sr.y;
+                        v.z *= sr.z;
+                        child.transform.localScale = v;
                     }
-                }
 
-                foreach (var col in obj.GetComponents<Collider2D>()) {
-                    var colOffset = col.offset;
-                    colOffset.Scale(oldScale);
-                    col.offset = colOffset;
-                    switch (col) {
-                        case BoxCollider2D box:
-                            Vector2 size = box.size;
-                            size.Scale(oldScale);
-                            box.size = size;
-                            break;
-                        case CapsuleCollider2D capsule:
-                            size = capsule.size;
-                            size.Scale(oldScale);
-                            capsule.size = size;
-                            break;
-                        case EdgeCollider2D edge:
-                            edge.points = ScalePoints(edge.points, oldScale);
-                            break;
-                        case PolygonCollider2D poly:
-                            poly.points = ScalePoints(poly.points, oldScale);
-                            break;
+                    foreach (var col in obj.GetComponents<Collider>()) {
+                        var colOffset = GetColliderOffset(col);
+                        colOffset.Scale(oldScale);
+                        SetColliderOffset(col, colOffset);
+                        switch (col) {
+                            case BoxCollider box:
+                                Vector3 size = box.size;
+                                size.Scale(oldScale);
+                                box.size = size;
+                                break;
+                            case SphereCollider sphere:
+                                sphere.radius *= Mathf.Max(oldScale.x, oldScale.y, oldScale.z);
+                                break;
+                            case CapsuleCollider capsule:
+                                Vector3 dir = GetCapsuleDirection(capsule.direction);
+                                capsule.height *= Vector3.Dot(oldScale, dir);
+                                Vector3 v = Vector3.ProjectOnPlane(oldScale, dir);
+                                capsule.radius *= Mathf.Max(v.x, v.y, v.z);
+                                break;
+                        }
+                    }
+
+                    foreach (var col in obj.GetComponents<Collider2D>()) {
+                        var colOffset = col.offset;
+                        colOffset.Scale(oldScale);
+                        col.offset = colOffset;
+                        switch (col) {
+                            case BoxCollider2D box:
+                                Vector2 size = box.size;
+                                size.Scale(oldScale);
+                                box.size = size;
+                                break;
+                            case CapsuleCollider2D capsule:
+                                size = capsule.size;
+                                size.Scale(oldScale);
+                                capsule.size = size;
+                                break;
+                            case EdgeCollider2D edge:
+                                edge.points = ScalePoints(edge.points, oldScale);
+                                break;
+                            case PolygonCollider2D poly:
+                                poly.points = ScalePoints(poly.points, oldScale);
+                                break;
+                        }
                     }
                 }
             }
@@ -283,7 +284,7 @@ namespace SBR.Editor {
             Undo.RecordObjects(objs.ToArray(), message);
         }
 
-        private static Quaternion GetScaleMapping(Quaternion rotation) {
+        private static Matrix4x4 GetScaleMapping(Quaternion rotation) {
             Vector3 y = rotation * Vector3.up;
             Vector3 z = rotation * Vector3.forward;
 
@@ -313,9 +314,12 @@ namespace SBR.Editor {
                 up = Vector3.forward;
             }
 
-            return Quaternion.LookRotation(fwd, up);
+            Quaternion q = Quaternion.LookRotation(fwd, up);
+            Matrix4x4 m = Matrix4x4.Rotate(q);
+            if (m.m20 < 0) m.m20 *= -1;
+            return m;
         }
-        
+
         [MenuItem("GameObject/Apply Transform/All")]
         public static void ApplyAll() {
             ApplyPosition();
