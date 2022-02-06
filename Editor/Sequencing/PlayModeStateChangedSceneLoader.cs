fef6823678d47ff1a23c5a9bb2278814008eda7d
Vincent Miller <vmiller0725@gmail.com>
2022-02-06 13:31:21 -0800

    Expose some of the scene loading functionality so that it can be used without the project setting.

--- Editor/Sequencing/PlayModeStateChangedSceneLoader.cs
+++ Editor/Sequencing/PlayModeStateChangedSceneLoader.cs
@@ -20,48 +20,56 @@ namespace SBR.Editor.Sequencing {
         private static void EditorApplication_PlayModeStateChanged(PlayModeStateChange mode) {
             if (!SBRProjectSettings.inst.autoLoadScene0InEditor) return;
             if (mode == PlayModeStateChange.ExitingEditMode) {
-                EditorSceneManager.SaveOpenScenes();
-                EditorPrefs.SetString("ActiveScene", SceneManager.GetActiveScene().path);
-                EditorPrefs.SetInt("OpenSceneCount", SceneManager.sceneCount);
-                for (int i = 0; i < SceneManager.sceneCount; i++) {
-                    var scene = SceneManager.GetSceneAt(i);
-                    EditorPrefs.SetString($"OpenScenes[{i}].name", scene.path);
-                    EditorPrefs.SetBool($"OpenScenes[{i}].loaded", scene.isLoaded);
-                }
-
-                EditorPrefs.SetInt("OpenLevel", -1);
-                foreach (var level in LevelManifest.Instance.Levels) {
-                    bool isLoaded = SceneManager.GetSceneByPath(level.Scene.Path).isLoaded;
-                    foreach (LevelManifestRegionEntry region in level.Regions) {
-                        bool isRegionLoaded = SceneManager.GetSceneByPath(region.Scene.Path).isLoaded;
-                        EditorPrefs.SetBool($"OpenLevelRegions[{region.RegionID}].loaded", isRegionLoaded);
-                        if (isRegionLoaded) isLoaded = true;
-                    }
-
-                    if (isLoaded) {
-                        EditorPrefs.SetInt("OpenLevel", level.LevelID);
-                        break;
-                    }
-                }
-
-                EditorSceneManager.OpenScene(EditorBuildSettings.scenes[0].path, OpenSceneMode.Single);
+                LoadFromInitialScene();
             } else if (mode == PlayModeStateChange.EnteredEditMode) {
-                int count = EditorPrefs.GetInt("OpenSceneCount", 0);
-                
-                for (int i = 0; i < count; i++) {
-                    string name = EditorPrefs.GetString($"OpenScenes[{i}].name", null);
-                    bool isLoaded = EditorPrefs.GetBool($"OpenScenes[{i}].loaded", true);
-                    if (string.IsNullOrEmpty(name)) continue;
-                    
-                    EditorSceneManager.OpenScene(name, i == 0 ? OpenSceneMode.Single : OpenSceneMode.Additive);
-                    if (!isLoaded) {
-                        var loadedScene = SceneManager.GetSceneByName(name);
-                        EditorSceneManager.CloseScene(loadedScene, false);
-                    }
+                ReloadEditingScenes();
+            }
+        }
+
+        public static void LoadFromInitialScene() {
+            EditorSceneManager.SaveOpenScenes();
+            EditorPrefs.SetString("ActiveScene", SceneManager.GetActiveScene().path);
+            EditorPrefs.SetInt("OpenSceneCount", SceneManager.sceneCount);
+            for (int i = 0; i < SceneManager.sceneCount; i++) {
+                var scene = SceneManager.GetSceneAt(i);
+                EditorPrefs.SetString($"OpenScenes[{i}].name", scene.path);
+                EditorPrefs.SetBool($"OpenScenes[{i}].loaded", scene.isLoaded);
+            }
+
+            EditorPrefs.SetInt("OpenLevel", -1);
+            foreach (var level in LevelManifest.Instance.Levels) {
+                bool isLoaded = SceneManager.GetSceneByPath(level.Scene.Path).isLoaded;
+                foreach (LevelManifestRegionEntry region in level.Regions) {
+                    bool isRegionLoaded = SceneManager.GetSceneByPath(region.Scene.Path).isLoaded;
+                    EditorPrefs.SetBool($"OpenLevelRegions[{region.RegionID}].loaded", isRegionLoaded);
+                    if (isRegionLoaded) isLoaded = true;
                 }
 
-                SceneManager.SetActiveScene(SceneManager.GetSceneByPath(EditorPrefs.GetString("ActiveScene", null)));
+                if (isLoaded) {
+                    EditorPrefs.SetInt("OpenLevel", level.LevelID);
+                    break;
+                }
             }
+
+            EditorSceneManager.OpenScene(EditorBuildSettings.scenes[0].path, OpenSceneMode.Single);
+        }
+
+        public static void ReloadEditingScenes() {
+            int count = EditorPrefs.GetInt("OpenSceneCount", 0);
+
+            for (int i = 0; i < count; i++) {
+                string name = EditorPrefs.GetString($"OpenScenes[{i}].name", null);
+                bool isLoaded = EditorPrefs.GetBool($"OpenScenes[{i}].loaded", true);
+                if (string.IsNullOrEmpty(name)) continue;
+
+                EditorSceneManager.OpenScene(name, i == 0 ? OpenSceneMode.Single : OpenSceneMode.Additive);
+                if (!isLoaded) {
+                    var loadedScene = SceneManager.GetSceneByName(name);
+                    EditorSceneManager.CloseScene(loadedScene, false);
+                }
+            }
+
+            SceneManager.SetActiveScene(SceneManager.GetSceneByPath(EditorPrefs.GetString("ActiveScene", null)));
         }
     }
 }
