1923bd6f41eab9cce4e8183b9af06d94a85ae24e
Vincent Miller <vmiller0725@gmail.com>
2022-02-06 11:33:07 -0800

    Time scaler component

--- Sequencing/LevelManifestLevelEntry.cs
+++ Sequencing/LevelManifestLevelEntry.cs
@@ -9,14 +9,14 @@ namespace SBR.Sequencing {
         [SerializeField] private int _levelID;
         [SerializeField] private SceneRef _scene;
 
-        [SerializeField] [Expandable(false)]
+        [SerializeField, NonReorderable, Expandable]
         private LevelManifestRegionEntry[] _regions;
 
         public string DisplayName => _displayName;
         public int LevelID => _levelID;
         public SceneRef Scene => _scene;
         public IReadOnlyList<LevelManifestRegionEntry> Regions => _regions;
-        
+
         public LevelManifestRegionEntry GetRegionWithID(int id) {
             return _regions.FirstOrDefault(r => r.RegionID == id);
         }
---
+++ Util/Misc/TimeScaleSlider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace SBR {
+    public class TimeScaleSlider : MonoBehaviour {
+        [Range(0, 1)] [SerializeField] private float _timeScale = 1;
+
+        private float _lastTimeScale;
+
+        private void Start() {
+            _lastTimeScale = _timeScale;
+        }
+
+        private void LateUpdate() {
+            if (_timeScale != _lastTimeScale) {
+                Time.timeScale = _timeScale;
+                Time.fixedDeltaTime = _timeScale / 60.0f;
+            }
+
+            _lastTimeScale = _timeScale = Time.timeScale;
+        }
+    }
+}
