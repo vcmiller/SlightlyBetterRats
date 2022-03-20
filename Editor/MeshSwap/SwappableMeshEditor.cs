c407f0fe168b94fa2768104236f286abccd05d90
Vincent Miller <vmiller0725@gmail.com>
2022-03-20 00:16:23 -0700

    Button to refresh swappable mesh manually.

---
+++ Editor/MeshSwap/SwappableMeshEditor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace SBR.Editor {
+    [CustomEditor(typeof(SwappableMesh)), CanEditMultipleObjects]
+    public class SwappableMeshEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+            if (GUILayout.Button("Refresh")) {
+                foreach (Object t in targets) {
+                    ((SwappableMesh)t).RefreshModel();
+                }
+            }
+        }
+    }
+}
