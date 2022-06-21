ee6654fbe08ccf5a9f4af0b257072341333b5f3e
Vincent Miller <vmiller0725@gmail.com>
2022-06-21 01:45:20 -0700

    Unique names now support null entry.

--- Util/UniqueName/UniqueNamedObject.cs
+++ Util/UniqueName/UniqueNamedObject.cs
@@ -11,6 +11,8 @@ namespace SBR {
         private static Dictionary<string, UniqueNamedObject> _objects =
             new Dictionary<string, UniqueNamedObject>();
 
+        public static IReadOnlyDictionary<string, UniqueNamedObject> Objects => _objects;
+
         public static bool TryGetObject(string name, out GameObject result) {
             bool value = _objects.TryGetValue(name, out UniqueNamedObject obj);
             result = obj ? obj.gameObject : null;
@@ -25,10 +27,11 @@ namespace SBR {
         }
 
         private void Awake() {
-            UniqueName = _uniqueName.name;
+            UniqueName = _uniqueName ? _uniqueName.name : null;
         }
 
         private void OnEnable() {
+            if (UniqueName == null) return;
             if (_objects.TryGetValue(UniqueName, out UniqueNamedObject other)) {
                 Debug.LogError(
                     $"Object {name} trying to use unique name {UniqueName} which is already in use by {other.name}");
@@ -39,6 +42,7 @@ namespace SBR {
         }
 
         private void OnDisable() {
+            if (UniqueName == null) return;
             if (_objects[UniqueName] != this) return;
             _objects[UniqueName] = null;
         }
