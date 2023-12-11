0cd46e188bd24b38292d62a36e0e3153550c6c95
Vincent Miller <vmiller0725@gmail.com>
2023-12-11 00:43:10 -0800

    Fixed pause menu bug.

--- Runtime/Menu/PauseMenu.cs
+++ Runtime/Menu/PauseMenu.cs
@@ -1,17 +1,17 @@
 ﻿// The MIT License (MIT)
-// 
+//
 // Copyright (c) 2022-present Vincent Miller
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,7 +35,7 @@ namespace SBR.Menu {
 
         public UnityEvent onOpen;
         public UnityEvent onClose;
-        
+
         public bool IsOpen { get; private set; }
 
         private CursorLockMode cursorLockState;
@@ -60,13 +60,15 @@ namespace SBR.Menu {
         private void Update() {
             if (pauseAction != null &&
                 pauseAction.action.WasPerformedThisFrame() &&
-                !Infohazard.Core.Pause.Paused && (!target || !target.show) &&
+                !Infohazard.Core.Pause.Paused &&
+                !IsOpen &&
                 pauseTimer.canUse) {
 
                 Pause();
             } else if (resumeAction != null &&
                 resumeAction.action.WasPerformedThisFrame() &&
-                Infohazard.Core.Pause.Paused && (!target || target.show) &&
+                Infohazard.Core.Pause.Paused &&
+                IsOpen &&
                 pauseTimer.canUse) {
 
                 Unpause();
